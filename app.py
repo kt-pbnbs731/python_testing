864b575274c1de03dd76b7bfee3ab210c5480252
kt-pbnbs731 <59944761+kt-pbnbs731@users.noreply.github.com>
2020-01-16 12:58:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ port = int(os.environ.get('PORT', 5000))
 @app.route('/')
 def hello():
     # return 'Welcome to Python on Unubo Cloud'
-    return "!!! hello world !!!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
